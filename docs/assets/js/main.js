616df141f4f5be9b1107cc4f8819576748a71a8c
shoen1x <mohdmuizz22@yahoo.com>
2022-12-17 03:15:53 +0800

    Major Update
    Fix latest error/bugs, added but removed snap scroll because of clashes snap with footer, added new images scroller (WIP for automated scroll over hover, re-added works display

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,8 +1,6 @@
-/*
-	Big Picture by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+//	Revenant Formal by NMVX
+//	Shoen1x.github.io | @Shoenixstudios
+//  under the CCA 3.0 License | Credit to HTML5 UP for template
 
 (function($) {
 
@@ -232,3 +230,26 @@
 			});
 
 })(jQuery);
+
+// Hero Slider
+$('.slide-nav').on('click', function(e) {
+	e.preventDefault();
+	// get current slide
+	var current = $('.flex--active').data('slide'),
+	  // get button data-slide
+	  next = $(this).data('slide');
+  
+	$('.slide-nav').removeClass('active');
+	$(this).addClass('active');
+  
+	if (current === next) {
+	  return false;
+	} else {
+	  $('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
+	  $('.flex--active').addClass('animate--end');
+	  setTimeout(function() {
+		$('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+		$('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+	  }, 800);
+	}
+  });
