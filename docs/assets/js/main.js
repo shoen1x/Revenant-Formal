f7740bde197d4c1feb7a421ea8a3a6d94c937e9e
shoen1x <mohdmuizz22@yahoo.com>
2022-12-20 03:48:24 +0800

    Video player updated
    Updated video player embedded youtube with html 5 player with progressive enhancement.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -252,4 +252,52 @@
 		}
 	});
 
+// Plyr.io Video Player
+//const player = new Plyr('#player');
+
+document.addEventListener('DOMContentLoaded', () => {
+	const controls = [
+	   'play-large', // The large play button in the center
+	   'restart', // Restart playback
+	   //'rewind', // Rewind by the seek time (default 10 seconds)
+	   'play', // Play/pause playback
+	   //'fast-forward', // Fast forward by the seek time (default 10 seconds)
+	   'progress', // The progress bar and scrubber for playback and buffering
+	   //'current-time', // The current time of playback
+	   'duration', // The full duration of the media
+	   'mute', // Toggle mute
+	   'volume', // Volume control
+	   //'captions', // Toggle captions
+	   //'settings', // Settings menu
+	   //'pip', // Picture-in-picture (currently Safari only)
+	   //'airplay', // Airplay (currently Safari only)
+	   //'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+	   'fullscreen' // Toggle fullscreen
+   ];
+   const player = Plyr.setup('.js-player', { controls });
+});
+
+
+
 })(jQuery);
+
+jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+jQuery.event.special.touchmove = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+jQuery.event.special.wheel = {
+    setup: function( _, ns, handle ){
+        this.addEventListener("wheel", handle, { passive: true });
+    }
+};
+jQuery.event.special.mousewheel = {
+    setup: function( _, ns, handle ){
+        this.addEventListener("mousewheel", handle, { passive: true });
+    }
+};
