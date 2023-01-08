b2eb4dfd0b78cbb0d050fdf92c4bab54456e9ef7
Shoenix <mohdmuizz22@yahoo.com>
2023-01-08 22:06:15 +0800

    Mobile Experience optimized
    Re-introduce mobile experience, removed redundance and fix all mobile frames incorrect aspect ratio in CSS

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -4,16 +4,16 @@
 
 (function ($) {
 
-	// Disable Mobile Experiences
-	function isMobile() {
-		return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|android|ipad|playbook|silk/i.test(navigator.userAgent || navigator.vendor || window.opera) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test((navigator.userAgent || navigator.vendor || window.opera).substr(0, 4)))
-	}
+	// // Disable Mobile Experiences
+	// function isMobile() {
+	// 	return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|android|ipad|playbook|silk/i.test(navigator.userAgent || navigator.vendor || window.opera) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test((navigator.userAgent || navigator.vendor || window.opera).substr(0, 4)))
+	// }
 
-	jQuery.noConflict();
-	jQuery(document).ready(function () {
-		if (isMobile()) document.getElementsByTagName('body')[0].style.display = 'none', document.title = 'Mobile experience not working';
-		else console.log('Hello user!');
-	});
+	// jQuery.noConflict();
+	// jQuery(document).ready(function () {
+	// 	if (isMobile()) document.getElementsByTagName('body')[0].style.display = 'none', document.title = 'Mobile experience not working';
+	// 	else console.log('Hello user!');
+	// });
 
 	var $window = $(window),
 		$body = $('body'),
@@ -240,60 +240,6 @@
 			$window.trigger('resize');
 		});
 
-	// Hero Slider
-	$('.slide-nav').on('click', function (e) {
-		e.preventDefault();
-		// get current slide
-		var current = $('.flex--active').data('slide'),
-			// get button data-slide
-			next = $(this).data('slide');
-
-		$('.slide-nav').removeClass('active');
-		$(this).addClass('active');
-
-		if (current === next) {
-			return false;
-		} else {
-			$('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
-			$('.flex--active').addClass('animate--end');
-			setTimeout(function () {
-				$('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
-				$('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
-			}, 800);
-		}
-	});
-
-	// Images Timer Scroll
-	setInterval(function () {
-		var current = $('.flex--active').data('slide'), next = $('.slide-nav').data('slide');
-
-		next = current + 1;
-		$('.slide-nav').removeClass('active');
-		$(this).addClass('active');
-
-		if (next == 6) {
-			current = 1;
-			next = 1;
-			$('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
-			$('.flex--active').addClass('animate--end');
-			setTimeout(function () {
-				$('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
-				$('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
-			}, 800);
-		} else {
-			if (current === next) {
-				return false;
-			} else {
-				$('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
-				$('.flex--active').addClass('animate--end');
-				setTimeout(function () {
-					$('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
-					$('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
-				}, 800);
-			}
-		}
-	}, 15000);
-
 	// Splide base Ads carousel
 	var splide;
 
@@ -344,10 +290,9 @@
 			$('#pdisplay').removeClass('hidden');
 
 			document.querySelector('#header h1').innerHTML = 'Southern Tigers Concept'
-			document.querySelector('.project').innerHTML = 'Southern Tigers Concept';
+			document.querySelector('.project').innerHTML = 'Southern Tigers<br>Concept';
 			document.querySelector('.project-description').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptate, laboriosam sunt eveniet eius iure, sapiente in maiores quasi saepe quas quisquam obcaecati odio ';
 			document.querySelector('.project-overview').innerHTML = 'Southern Tigers Lorem';
-			document.querySelector('.wrap').innerHTML = '<image-parallax sp="60" class="el1" src="images/fulls/full/st1.webp"></image-parallax><image-parallax sp="100" class="el2" src="images/fulls/full/st2.webp"></image-parallax><image-parallax sp="-80" class="el3"><img src="images/fulls/full/st3.webp"/></image-parallax>';
 
 			document.querySelector('.center-image-background').innerHTML = `
 			<img class="center-image-parallax" src="images/fulls/full/st7.webp" />
@@ -375,11 +320,9 @@
 			$('#pdisplay').removeClass('hidden');
 
 			document.querySelector('#header h1').innerHTML = 'Istinggar'
-			document.querySelector('.project').innerHTML = 'Istinggar Concept';
+			document.querySelector('.project').innerHTML = 'Istinggar<br>Concept';
 			document.querySelector('.project-description').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptate, laboriosam sunt eveniet eius iure, sapiente in maiores quasi saepe quas quisquam obcaecati odio ';
 			document.querySelector('.project-overview').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptate, laboriosam sunt eveniet eius iure, sapiente in maiores quasi saepe quas quisquam obcaecati odio ';
-			document.querySelector('.wrap').innerHTML = '<image-parallax sp="60" class="el1" src="images/fulls/full/ic1.webp"></image-parallax><image-parallax sp="100" class="el2" src="images/fulls/full/ic2.webp"></image-parallax><image-parallax sp="-80" class="el3"><img src="images/fulls/full/ic3.webp"/></image-parallax>';
-
 			document.querySelector('.center-image-background').innerHTML = `
 			<img class="center-image-parallax" src="images/fulls/full/ic1.webp" />
 			<img class="center-image-parallax" src="images/fulls/full/ic2.webp" />
@@ -403,13 +346,9 @@
 			$('#pdisplay').removeClass('hidden');
 
 			document.querySelector('#header h1').innerHTML = 'Aerophantom Earbuds'
-			document.querySelector('.project').innerHTML = 'Aerophantom Earbuds';
+			document.querySelector('.project').innerHTML = 'Aerophantom<br>Earbuds';
 			document.querySelector('.project-description').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptate, laboriosam sunt eveniet eius iure, sapiente in maiores quasi saepe quas quisquam obcaecati odio ';
 			document.querySelector('.project-overview').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptate, laboriosam sunt eveniet eius iure, sapiente in maiores quasi saepe quas quisquam obcaecati odio ';
-			document.querySelector('.wrap').innerHTML = `
-			<image-parallax sp="60" class="el1" src="images/fulls/full/ae4.webp"></image-parallax>
-			<image-parallax sp="100" class="el2" src="images/fulls/full/ae2.webp"></image-parallax>
-			<image-parallax sp="-80" class="el3" src="images/fulls/full/ae3.webp"></image-parallax>`;
 
 			document.querySelector('.center-image-background').innerHTML = `
 			<img class="center-image-parallax" src="images/fulls/full/ae1.webp" />
@@ -430,13 +369,9 @@
 			$('#pdisplay').removeClass('hidden');
 
 			document.querySelector('#header h1').innerHTML = 'Nordic Axe Concept'
-			document.querySelector('.project').innerHTML = 'Nordic Axe Concept';
+			document.querySelector('.project').innerHTML = 'Nordic Axe<br>Concept';
 			document.querySelector('.project-description').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptate, laboriosam sunt eveniet eius iure, sapiente in maiores quasi saepe quas quisquam obcaecati odio ';
 			document.querySelector('.project-overview').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptate, laboriosam sunt eveniet eius iure, sapiente in maiores quasi saepe quas quisquam obcaecati odio ';
-			document.querySelector('.wrap').innerHTML = `
-			<image-parallax sp="60" class="el1" src="images/fulls/full/na1.webp"></image-parallax>
-			<image-parallax sp="100" class="el2" src="images/fulls/full/na5.webp"></image-parallax>
-			<image-parallax sp="-80" class="el3" src="images/fulls/full/na4.webp"></image-parallax>`;
 
 			document.querySelector('.center-image-background').innerHTML = `
 			<img class="center-image-parallax" src="images/fulls/full/na1.webp" />
@@ -459,13 +394,9 @@
 			$('#pdisplay').removeClass('hidden');
 
 			document.querySelector('#header h1').innerHTML = 'Harimau Malaya'
-			document.querySelector('.project').innerHTML = 'Harimau Malaya';
+			document.querySelector('.project').innerHTML = 'Harimau<br>Malaya';
 			document.querySelector('.project-description').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptate, laboriosam sunt eveniet eius iure, sapiente in maiores quasi saepe quas quisquam obcaecati odio ';
 			document.querySelector('.project-overview').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptate, laboriosam sunt eveniet eius iure, sapiente in maiores quasi saepe quas quisquam obcaecati odio ';
-			document.querySelector('.wrap').innerHTML = `
-			<image-parallax sp="60" class="el1" src="images/fulls/full/hm1.webp"></image-parallax>
-			<image-parallax sp="100" class="el2" src="images/fulls/full/hm4.webp"></image-parallax>
-			<image-parallax sp="-80" class="el3" src="images/fulls/full/hm6.webp"></image-parallax>`;
 
 			document.querySelector('.center-image-background').innerHTML = `
 			<img class="center-image-parallax" src="images/fulls/full/hm1.webp" />
@@ -488,7 +419,7 @@
 	})
 
 	// On focus top file
-	$('#navsection').on('mouseover mouseout', function () {
+	$('#navsection').on('mouseover mouseout touchstart', function () {
 		if (document.documentElement.scrollTop == 0) {
 			$('#pdisplay').addClass('hidden');
 			document.querySelector('#header h1').innerHTML = 'Projects'
