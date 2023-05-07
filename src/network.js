10a99ac648d5e12f8ca2de2df16f4c25c4f745fc
shoen1x <mohdmuizz22@yahoo.com>
2023-05-07 23:47:29 +0800

    RevF1683474044000
    Loop Bug hotfixes

--- src/network.js
+++ src/network.js
@@ -24,6 +24,7 @@ const net_path = 'https://shoenix-studios.web.app/';
 	}
 
     var langStr;
+    var net_path_slicer  = net_path + getCookie('firebase-language-override') + "/" + location.pathname.split("/").slice(-1);
     if (document.cookie.indexOf("firebase-language-override=") >= 0) {
         langStr = getCookie('firebase-language-override');
         countryStr = getCookie('firebase-country-override');
@@ -35,11 +36,14 @@ const net_path = 'https://shoenix-studios.web.app/';
             // .filter('[value=' + document.cookie.substring(langck + 5) + ']')
             .attr('selected', true);
         langStr = '[firebase-language-override="' + langStr + '"]';
-        if(getCookie('firebase-language-override') == 'ms'){
-            window.location.href = net_path + getCookie('firebase-language-override') + "/" + location.pathname.split("/").slice(-1);
-        }else{
-            window.location.href = net_path + location.pathname.split("/").slice(-1);
-        }
+        $( document ).ready(function(){
+            console.log(window.location.href);
+            if(getCookie('firebase-language-override') == 'ms' && window.location.href != net_path_slicer){
+                window.location.href = net_path + getCookie('firebase-language-override') + "/" + location.pathname.split("/").slice(-1);
+            }else if(getCookie('firebase-language-override') == 'en' && window.location.href != net_path_slicer){
+                window.location.href = net_path + location.pathname.split("/").slice(-1);
+            }
+         });
         // Utk debug jah
     } else {
         // No cookie - tunjuk inggeris
